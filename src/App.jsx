cdf990dc90c999ad4e791688bbbfa810dca1ee58
kimsujin <88781500+kimsujin115@users.noreply.github.com>
2022-11-02 00:42:01 +0900

    Add files via upload
    ref 사용성 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import Main from './pages/components/Main';
 import Category from './pages/components/Category';
 import Detail from './pages/components/Detail';
 
+//ref 사용성 테스트
+import TestRef from './pages/components/TestRef';
+
 import './asset/scss/app.scss'
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
           <Route exact path="/category/:index" element={<Category/>}></Route>
           {/* 추후에 useParams() 함수 메서드를 통해서 던질값에 대해서는 변수명을 일치시킬 것 */}
           <Route exact path='/detail/:index' element={<Detail/>}></Route>
+
+          {/* ref 사용성 테스트 */}
+          <Route exact path='/test' element={<TestRef/>}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
